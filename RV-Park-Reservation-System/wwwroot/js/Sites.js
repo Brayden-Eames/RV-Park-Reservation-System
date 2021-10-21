768ae8c2ad5d55b67580b3a1998a1cda43b44f8c
KylerHansen <kylerhansen@mail.weber.edu>
2021-10-21 09:52:46 -0600

    Basic table done

---
+++ RV-Park-Reservation-System/wwwroot/js/Sites.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/sites",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { data: "site_Category.siteCategoryName", width: "15%" },
+            { data: "siteNumber", width: "15%" },
+            { data: "siteLength", width: "10%" }, 
+            { data: "siteDescription", width: "20%" },
+            { data: "siteLastModifiedDate", width: "15%" },
+            {
+                data: "siteID", width: "25%",
+                "render": function (data) {
+                    return `<div class="text-center">
+                            <a href="/Admin/MenuItems/Upsert?id=${data}"
+                            class ="btn btn-success text-white style="cursor:pointer; width=100px;"> <i class="far fa-edit"></i> Edit</a>                            
+                            <a onClick=Delete('/api/sites/'+${data})
+                            class ="btn btn-danger text-white style="cursor:pointer; width=100px;"> <i class="far fa-trash-alt"></i> Delete</a>
+                    </div>`;
+                }
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore this data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else
+                        toastr.error(data.message);
+                }
+            })
+        }
+    })
+}
