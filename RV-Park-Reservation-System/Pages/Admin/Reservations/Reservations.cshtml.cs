9a14792877a1ccbb0e1027e668554eb4c345ddf5
Brayden Eames <braydeneames@ymail.com>
2021-10-21 17:26:08 -0600

    Added AdminReservationVM view model, wrote backend code to display reservations in the Reservation admin page.

--- RV-Park-Reservation-System/Pages/Admin/Reservations/Reservations.cshtml.cs
+++ RV-Park-Reservation-System/Pages/Admin/Reservations/Reservations.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApplicationCore.Interfaces;
 using ApplicationCore.Models;
+using RV_Park_Reservation_System.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -16,9 +17,23 @@ namespace RV_Park_Reservation_System.Pages.Admin
 
         public IEnumerable<Reservation> ReservationList { get; set; }
 
+        public AdminReservationVM AdminReservationObject { get; set; }
+
         //We might need to make a ViewModel to allow us to pull from the Reservation, Customer, Service Status Type and DODAffiliation tables. 
         public void OnGet()
         {
+            //need to pull data from Reservation, Customer, DODAffiliation and ServiceStatusType tables. Use the ViewModel to do so
+            ReservationList = _unitofWork.Reservation.List();
+            AdminReservationObject = new AdminReservationVM()
+            {
+                Reservations = _unitofWork.Reservation.List(),
+                ListOfCustomers = _unitofWork.Customer.List(),
+                DODAffiliationList = _unitofWork.DOD_Affiliation.List(),
+                ServiceStatusTypes = _unitofWork.Service_Status_Type.List()
+            };
+
+            //IMPORTANT: NEED TO MAKE LINQ QUERY (Copy admin methodology from JohariWindow) TO DISPLAY PROPER DATA
+            //Possible Idea: use admin vm page object data to populate lists for the LINQ query
         }
     }
 }
