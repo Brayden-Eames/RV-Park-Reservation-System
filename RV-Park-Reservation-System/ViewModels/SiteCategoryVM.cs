0c525e0d0112425bca081ecf61a38619f1aaf059
rweston03 <randi.l.weston@gmail.com>
2021-10-26 12:21:33 -0600

    Added view models

---
+++ RV-Park-Reservation-System/ViewModels/SiteCategoryVM.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RV_Park_Reservation_System.ViewModels
+{
+    public class SiteCategoryVM
+    {
+        public Site_Category SiteCategory { get; set; }
+
+        public IEnumerable<SelectListItem>  LocationList { get; set; }
+    }
+}
---
+++ RV-Park-Reservation-System/ViewModels/SpecialEventVM.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RV_Park_Reservation_System.ViewModels
+{
+    public class SpecialEventVM
+    {
+        public Special_Event SpecialEvent { get; set; }
+
+        public IEnumerable<SelectListItem>  LocationList { get; set; }
+    }
+}
