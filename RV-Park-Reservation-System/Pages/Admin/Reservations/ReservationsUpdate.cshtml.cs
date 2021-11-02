73a970de99f0ad27e76da057d044ec419a82754e
Brayden Eames <braydeneames@ymail.com>
2021-11-01 18:19:26 -0600

    Reservation Update and Delete fully functional! Removed ReservationsDelete razor page (changed functionality)

--- RV-Park-Reservation-System/Pages/Admin/Reservations/ReservationsUpdate.cshtml.cs
+++ RV-Park-Reservation-System/Pages/Admin/Reservations/ReservationsUpdate.cshtml.cs
@@ -38,26 +38,33 @@ namespace RV_Park_Reservation_System.Pages.Admin.Reservations
             
         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var reservation = _unitOfWork.Reservation.Get(c => c.ResID == CustomerReservation.ResID);
-
-            reservation.SiteID = CustomerReservation.SiteID;
-            reservation.ResStartDate = CustomerReservation.ResStartDate;
-            reservation.ResEndDate = CustomerReservation.ResEndDate;
-            reservation.ResNumAdults = CustomerReservation.ResNumAdults;
-            reservation.ResNumChildren = CustomerReservation.ResNumChildren;
-            reservation.ResNumPets = CustomerReservation.ResNumPets;
-            reservation.ResAcknowledgeValidPets = CustomerReservation.ResAcknowledgeValidPets;
-            reservation.ResComment = CustomerReservation.ResComment;
-            reservation.ResVehicleLength = CustomerReservation.ResVehicleLength;
-            reservation.Vehicle_Type = CustomerReservation.Vehicle_Type;
-            reservation.Reservation_Status = CustomerReservation.Reservation_Status;
-
-             _unitOfWork.Reservation.Update(reservation);
-             _unitOfWork.Commit();
-           
-            return RedirectToPage("./Reservations", new { success = true, message = "Update Successful" });
+        public async Task<IActionResult> OnPostAsync(string value)
+        { //current issue: value is not being sent to the page. Fix: Switch if checks to check for update instead of delete
+            if(value == "update") // if the delete button is clicked
+            {
+                var reservation = _unitOfWork.Reservation.Get(c => c.ResID == CustomerReservation.ResID);
+                reservation.SiteID = CustomerReservation.SiteID;
+                reservation.ResStartDate = CustomerReservation.ResStartDate;
+                reservation.ResEndDate = CustomerReservation.ResEndDate;
+                reservation.ResNumAdults = CustomerReservation.ResNumAdults;
+                reservation.ResNumChildren = CustomerReservation.ResNumChildren;
+                reservation.ResNumPets = CustomerReservation.ResNumPets;
+                reservation.ResAcknowledgeValidPets = CustomerReservation.ResAcknowledgeValidPets;
+                reservation.ResComment = CustomerReservation.ResComment;
+                reservation.ResVehicleLength = CustomerReservation.ResVehicleLength;
+                reservation.Vehicle_Type = CustomerReservation.Vehicle_Type;
+                reservation.Reservation_Status = CustomerReservation.Reservation_Status;
+                _unitOfWork.Reservation.Update(reservation);
+                _unitOfWork.Commit();
+                return RedirectToPage("./Reservations", new { success = true, message = "Update Successful" });
+            }
+            else
+            {
+                var reservation = _unitOfWork.Reservation.Get(c => c.ResID == CustomerReservation.ResID);
+                _unitOfWork.Reservation.Delete(reservation);
+                _unitOfWork.Commit();
+                return RedirectToPage("./Reservations", new { success = true, message = "Delete Successful" });
+            }
         }
     }
 }
