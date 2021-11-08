c89040591aa047694b57a7cff672a4eccd467be4
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-11-08 16:11:26 -0700

    Customer delete functionality

---
+++ RV-Park-Reservation-System/Pages/Admin/Users/Delete.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace RV_Park_Reservation_System.Pages.Admin.Users
+{
+    public class DeleteModel : PageModel
+    {
+        public string UserID { get; set; }
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly UserManager<Customer> _userManager;
+
+        public DeleteModel(IUnitOfWork unitOfWork, UserManager<Customer> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+
+        }
+
+
+        public void OnGet(string ID)
+        {
+            if (ID != null)
+            {
+                UserID = ID;
+                var customer = _unitOfWork.Customer.Get(u => u.Id == UserID);
+                _unitOfWork.Customer.Delete(customer);
+                Page();
+            }
+            else
+            {
+                RedirectToPage("/Admin/Users/Index");
+            }
+
+        }
+    }
+}
