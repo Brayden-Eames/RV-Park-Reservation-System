51c67b6c446f9faa49518128bfb8a8a5d8ff9c12
rweston03 <randi.l.weston@gmail.com>
2021-11-14 22:09:27 -0700

    Fixed Back to List links on data tables. Working on Dashboard.

--- RV-Park-Reservation-System/Pages/Admin/Index.cshtml.cs
+++ RV-Park-Reservation-System/Pages/Admin/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,11 +12,43 @@ namespace RV_Park_Reservation_System.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [BindProperty]
+        public List<Site> vacancies { get; set; }
+        [BindProperty]
+        public List<Site> occupancies { get; set; }
+
         public IActionResult OnGet()
         {
             if (!User.Identity.IsAuthenticated || User.IsInRole(SD.CustomerRole))
             {
                 return RedirectToPage("/Shared/Prohibited", new { path = "/Admin/Index" });
+            }
+            vacancies = new List<Site>();
+            occupancies = new List<Site>();
+
+            DateTime today = DateTime.Now.Date;
+
+            IEnumerable<Reservation> reservations = _unitOfWork.Reservation.List();
+
+            reservations = reservations.Where(s => (DateTime.Compare(s.ResStartDate, today) >=0 && DateTime.Compare(s.ResEndDate, today) >= 0) /*||
+                                                 ((s.ResStartDate > today && s.ResEndDate > today) && (s.ResStartDate < today && s.ResEndDate < today))*/);
+
+            IEnumerable<Site> sites = _unitOfWork.Site.List();
+
+            foreach(Site s in sites)
+            {
+                if (reservations.Where(r => r.SiteID == s.SiteID).Count() == 0)
+                {
+                    vacancies.Add(s);
+                }
+                else
+                {
+                    occupancies.Add(s);
+                }
             }
 
             return Page();
