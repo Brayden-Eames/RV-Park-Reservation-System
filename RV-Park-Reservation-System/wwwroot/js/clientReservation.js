b3b6c20d0f7b39ac2ac54525f819dda541bff1cc
rweston03 <randi.l.weston@gmail.com>
2021-12-06 15:18:04 -0700

    Fixes for mobile styling

--- RV-Park-Reservation-System/wwwroot/js/clientReservation.js
+++ RV-Park-Reservation-System/wwwroot/js/clientReservation.js
@@ -1,46 +1,79 @@
 ﻿var dataTable;
+var width = window.innerWidth;
+console.log(width)
 
 $(document).ready(function () {
 	loadList();
 });
 
 function loadList() {
-	dataTable = $('#DT_load').DataTable({
-		"ajax": {
-			"url": "/api/clientReservation",
-			"type": "GET",
-			"datatype": "json"
-		},
-		"columns": [
-			{ data: "resStartDate", width: "20%" },
-			{ data: "resEndDate", width: "20%" },
-			{ data: "resVehicleLength", width: "15%" },
-			{ data: "resNumAdults", width: "5%" },
-			{ data: "resNumChildren", width: "5%" },
-			{ data: "resNumPets", width: "5%" },
-			{
-				data: "resID", width: "30%",
-				"render": function (data) {
-					return `<div class="text-center">
+	if (width >= 990) {
+		dataTable = $('#DT_load').DataTable({
+			"ajax": {
+				"url": "/api/clientReservation",
+				"type": "GET",
+				"datatype": "json"
+			},
+			"columns": [
+				{ data: "resStartDate", width: "20%", "render": function (data) { return moment(data).format("MM/DD/YYYY, hh:mm a"); }  },
+				{ data: "resEndDate", width: "20%", "render": function (data) { return moment(data).format("MM/DD/YYYY, hh:mm a"); }  },
+				{ data: "resVehicleLength", width: "15%" },
+				{ data: "resNumAdults", width: "5%" },
+				{ data: "resNumChildren", width: "5%" },
+				{ data: "resNumPets", width: "5%" },
+				{
+					data: "resID", width: "30%",
+					"render": function (data) {
+						return `<div class="text-center">
                             <a href="/Client/Upsert?id=${data}"
                             class ="btn btn-success text-white" style="cursor:pointer; width=100px;"> <i class="far fa-edit"></i>Edit</a>
                             <a onClick=Delete('/api/clientReservation/'+${data})
                             class ="btn btn-danger text-white style="cursor:pointer; width=100px;"> <i class="far fa-trash-alt"></i>Cancel</a>
 							<a href="/Client/UpsertPayment?id=${data}"
-							class ="btn btn-info text-white" style="cursor:pointer; width=100px;"> <i class="far fa-edit"></i>Pay</a>
+							class ="btn btn-info text-white" style="cursor:pointer; width=100px;"> <i class="far fa-credit-card"></i>Pay</a>
                             </div>`;
-				},
+					},
 
-				
-			}
-		],
-		"language": {
-			"emptyTable": "no data found."
-		},
-		
-		"width": "100%"
-	});
-	
+
+				}
+			],
+			"language": {
+				"emptyTable": "no data found."
+			},
+
+			"width": "100%"
+		});
+	} else {
+		dataTable = $('#DT_load_mobile').DataTable({
+			"ajax": {
+				"url": "/api/clientReservation",
+				"type": "GET",
+				"datatype": "json"
+			},
+			"columns": [
+				{ data: "resStartDate", width: "20%", "render": function (data) { return moment(data).format("MM/DD/YYYY, hh:mm a"); }  },
+				{ data: "resEndDate", width: "20%", "render": function (data) { return moment(data).format("MM/DD/YYYY, hh:mm a"); }  },
+				{
+					data: "resID", width: "60%",
+					"render": function (data) {
+						return `<div class="text-center">
+                            <a href="/Client/Upsert?id=${data}"
+                            class ="btn btn-success text-white" style="cursor:pointer; width=100px;" data-toggle="tooltip" data-placement="top" title="Edit Reservation"> <i class="far fa-edit"></i></a>
+                            <a onClick=Delete('/api/clientReservation/'+${data})
+                            class ="btn btn-danger text-white style="cursor:pointer; width=100px;" data-toggle="tooltip" data-placement="top" title="Delete Reservation"> <i class="far fa-trash-alt"></i></a>
+							<a href="/Client/UpsertPayment?id=${data}"
+							class ="btn btn-info text-white" style="cursor:pointer; width=100px;" data-toggle="tooltip" data-placement="top" title="Make a Payment"> <i class="far fa-credit-card"></i></a>
+                            </div>`;
+					},
+				}
+			],
+			"language": {
+				"emptyTable": "no data found."
+			},
+
+			"width": "100%"
+		});
+    }
 }
 function Delete(url) {
 
